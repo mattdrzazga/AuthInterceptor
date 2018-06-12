826efdc62d3a010160a5c3b3a1eb25887d3cc17d
Mateusz Drzazga <matt.drzazga@gmail.com>
2018-06-12 18:59:08 +0200

    Add 2 methods for uploading attachment.

--- sample/src/main/java/com/matt/authinterceptor/sample/api/RestApiService.kt
+++ sample/src/main/java/com/matt/authinterceptor/sample/api/RestApiService.kt
@@ -1,14 +1,13 @@
 package com.matt.authinterceptor.sample.api
 
+import com.google.gson.JsonObject
 import com.matt.authinterceptor.HeaderWriter
 import com.matt.authinterceptor.sample.api.data.Credentials
 import com.matt.authinterceptor.sample.api.data.Profile
-
+import okhttp3.MultipartBody
 import retrofit2.Call
-import retrofit2.http.Body
-import retrofit2.http.GET
-import retrofit2.http.Headers
-import retrofit2.http.POST
+import retrofit2.http.*
+
 
 interface RestApiService {
 
@@ -18,4 +17,13 @@ interface RestApiService {
     @POST("profile")
     @Headers(HeaderWriter.NO_AUTH_HEADER)
     fun signUp(@Body credentials: Credentials): Call<Profile>
+
+    @Multipart
+    @POST("attachment")
+    @Headers(HeaderWriter.NO_AUTH_HEADER)
+    fun uploadAttachmentAsUser(@Part file: MultipartBody.Part): Call<JsonObject>
+
+    @Multipart
+    @POST("attachment")
+    fun uploadAttachmentAsGuest(@Part file: MultipartBody.Part): Call<JsonObject>
 }
